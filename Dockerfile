88a404c8ab42972fe46c1731beba6ba8a09e4cd2
Lu <lupa@inventati.org>
2016-07-25 15:46:33 -0400

    ajustes de config

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM tutum/lamp:latest
 
 ENV REPO https://github.com/TEDICpy/PAVotaciones.git
-ENV CARPETA /src
+ENV REPO2 https://github.com/TEDICpy/gs-share.git
+ENV CARPETA /app
 
-RUN rm -fr /app && mkdir /app
+# descargo el codigo
+RUN rm -fr $CARPETA \
+	&& git clone $REPO $CARPETA \
+	&& git clone $REPO2 $CARPETA/public/gs-share
 
-RUN git clone $REPO $CARPETA \
-	&& mv $CARPETA/public/* /app/
+# reCreo enlace para apache
+RUN rm /var/www/html \
+	&& ln -s $CARPETA/public /var/www/html	
 
 COPY ./votacionespa_latest.sql /
 COPY ./config.sh /
--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Creo la base y la cargo
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS votacionespa"
-mysql -u root votacionespa < votacionespa_latest.sql
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS votacionespa" \
+	&& mysql -u root votacionespa < votacionespa_latest.sql
 
 # Parametros correctos de conexion
-sed -i "s/publico/root/g" /app/server/conexion.php
-sed -i "s/\,\ \$db_password//g" /app/server/conexion.php
+sed -i "s/publico/root/g" /app/server/public/conexion.php
+sed -i "s/\,\ \$db_password//g" /app/server/public/conexion.php
