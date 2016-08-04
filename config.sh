96110443249a8b5a24548db0fdd64d0b026b3df7
Lu <lupa@inventati.org>
2016-08-04 16:10:09 -0300

    corrijo camino

--- config.sh
+++ config.sh
@@ -4,5 +4,5 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS votacionespa" \
 	&& mysql -u root votacionespa < votacionespa_latest.sql
 
 # Parametros correctos de conexion
-sed -i "s/publico/root/g" /app/server/public/conexion.php
-sed -i "s/\,\ \$db_password//g" /app/server/public/conexion.php
+sed -i "s/publico/root/g" /app/public/server/conexion.php
+sed -i "s/\,\ \$db_password//g" /app/public/server/conexion.php
